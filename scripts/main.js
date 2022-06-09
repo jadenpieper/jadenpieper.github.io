ad4c73d3048e68c2bcf29256c7ca720b53a3cebc
jadenpieper <jaden.pieper@gmail.com>
2022-06-09 11:28:12 -0600

    Commented out old stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent =  'Hello World!';
+
+
+//const myAlbums = document.querySelector('p');
+//myAlbums.textContent = 'Top albums list....'
+// const data = { username: 'example' };
+// fetch('https://api.deezer.com/user/4565334962/albums', {
+// 	method: 'GET',
+// })
+// .then(response => response.json())
+// .then(data => {
+// 	console.log('Succes:', data);
+// })
+// .catch((error) => {
+// 	console.error('Error:', error);
+// });
