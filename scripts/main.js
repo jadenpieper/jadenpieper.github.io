ddf84cbf634d2325093cca56d631dac837eeaec8
jadenpieper <jaden.pieper@gmail.com>
2022-06-10 14:51:18 -0600

    Added debug statement for with cors to track it

--- scripts/main.js
+++ scripts/main.js
@@ -101,7 +101,7 @@ function getAlbumsList(user_num, user_name_str, logged_in){
 	});
 }
 async function fetchAsync (url) {
-	console.log('Enter fetchAsync')
+	console.log('Enter fetchAsync w/ cors')
 	let response = await fetch(url, {method: 'POST', mode: 'cors'});
 	console.log(response)
 	let data = await response.json();
