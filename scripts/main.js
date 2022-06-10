e78a1cf6bca903ba584bedf49973b24f0936d0ea
jadenpieper <jaden.pieper@gmail.com>
2022-06-10 12:27:42 -0600

    Reorganized flow to hopefully allow login to be used

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,7 @@ if(current_url.hash){
 		DZ.api(api_user_call, function(response){
 			console.log(response)
 			user_num = response['id'];
+			getAlbumsList(response);
 		});
 	} else { 
 	console.log('Hash found but no user logged in')
@@ -63,36 +64,41 @@ function createTextSection(name, type){
 	return h2;
 }
 
-album_section = document.querySelector('#album-section')
+// album_section = document.querySelector('#album-section')
 
 
 console.log('user_num: ' + user_num)
 console.log('Logged in: ' + logged_in)
-// Display the user name
-let user_api_call = `/user/${user_num}`
-DZ.api(user_api_call, function(response){
-	user_name = response['name'];
+function getAlbumsList(user_response,){
+	user_num = user_response['id']
 	user_name = document.querySelector('#username');
 	user_name.textContent = response['name'];
-});
-// Set maximum number of albums to query
-// TODO: Make this a variable? Or increase if the number of albums in the query equals this
-let album_limit = 1000
+	// Display the user name
+	let user_api_call = `/user/${user_num}`
+	// DZ.api(user_api_call, function(response){
+// 		user_name = response['name'];
+// 		user_name = document.querySelector('#username');
+// 		user_name.textContent = response['name'];
+// 	});
+	// Set maximum number of albums to query
+	// TODO: Make this a variable? Or increase if the number of albums in the query equals this
+	let album_limit = 1000
 
-// Define the api call for getting the library in albums
-let api_call = `/user/${user_num}/albums&limit=${album_limit}`
-// console.log(api_call)
+	// Define the api call for getting the library in albums
+	let api_call = `/user/${user_num}/albums&limit=${album_limit}`
+	// console.log(api_call)
 
-DZ.api(api_call, function(response){
-	let total_albums = `Number of albums in library: ${response.total}`;
-	library_info = document.querySelector('#library_info');
-	library_info.textContent = total_albums;
-	const album_list = document.querySelector('#albums');
-	for (let ix =0; ix < 5; ix++) {
-		album_ix = Math.floor(Math.random() * response.total)
-		album = response.data[album_ix]
-		album_result = `${album['artist']['name']} - ${album['title']}`
-		album_list.appendChild(createAlbumItem(album_result))
-	}
+	DZ.api(api_call, function(response){
+		let total_albums = `Number of albums in library: ${response.total}`;
+		library_info = document.querySelector('#library_info');
+		library_info.textContent = total_albums;
+		const album_list = document.querySelector('#albums');
+		for (let ix =0; ix < 5; ix++) {
+			album_ix = Math.floor(Math.random() * response.total)
+			album = response.data[album_ix]
+			album_result = `${album['artist']['name']} - ${album['title']}`
+			album_list.appendChild(createAlbumItem(album_result))
+		}
 
-});
+	});
+}
